29b8068093d4ad5cc88495ffe8015979e840859c
rejeer <rejeercheriyandi@gamil.com>
2023-02-06 21:46:29 +0400

    Final file

---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+import './RowPost.css'
+import {imageUrl } from '../../constants/constants'
+import  axios  from '../../axios'
+import ReactPlayer from 'react-player'
+function RowPost(props) {
+  const[movies, setMovies]= useState([])
+  useEffect(() => {
+    axios.get(props.url).then(response => {
+      console.log(response.data)
+      setMovies(response.data.results)
+    }).catch(err => {
+     // alert('Network Error')
+    })
+  }, [])
+ 
+  return (
+    <div className='row'>
+      <h2>{props.title}</h2>
+      <div className='posters'>
+        {movies.map((obj)=>
+
+ <img className={props.isSmall ? 'smallPoster' :'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
+ )}
+ 
+       
+      </div>
+      <ReactPlayer  url='https://www.youtube.com/watch?v=viMq7SKebVA' />
+    </div>
+  )
+}
+
+export default RowPost
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+ export const baseUrl ='https://api.themoviedb.org/3';
+ export const API_KEY ='0299c1d678d6ca8f9b514b1d5c727976';
+export const imageUrl ='https://image.tmdb.org/t/p/original'
